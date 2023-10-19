8ef27f4e67a87236dd099e02fcb335804704c75f
Felipe Cruz <krivozznova@gmail.com>
2023-10-19 13:51:33 -0600

    feat(mobile): detecting mobile device and feeback response

---
+++ README.md
@@ -0,0 +1,17 @@
+# attendence cat
+
+## requirements
+python 11
+
+## installation
+- create a venv
+- install dependencies with pip install -r requirements.txt
+- use python main.py to start
+
+
+|Variable|Description| example |
+|--|--------|--:|
+|BOT_TOKEN| Discrod provided token |MTdfsazIGpQavS.asd.1OQmPwdL3N5t |
+|MONGO_HOST| The attendence use a persistence db for records | mongodb://localhost:27017/ |
+|MONGO_DB| The attendence database | attendence_db |
+|DISCORD_CHANNEL| The specific channel's name for attendence | attendence-record |
--- clients/discord_client.py
+++ clients/discord_client.py
@@ -1,5 +1,5 @@
 import discord
-
+import os
 from datetime import datetime
 
 from clients.mongo_client import insert_attendence
@@ -8,6 +8,8 @@ ATTENDENCE_CHANNEL = os.getenv('DISCORD_CHANNEL', None);
 
 COFFEE_EMOJI = u"\u2615"
 STATUS = ('available', 'unavailable')
+STATUS_OFFLINE = 'offline'
+STATUS_ONLINE = 'online'
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -20,7 +22,18 @@ async def on_message(message):
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
-    print(f'{username}: {user_message} ({channel})')
+
+    desktop_status = message.author.desktop_status
+    web_status = message.author.web_status
+    # print(f'{username}: {user_message} ({channel})')
+    # print(f'{message.author.status}')
+    # print(f'{message.author.is_on_mobile()}')
+
+    IS_JUST_ONLINE_IN_MOBILE = (
+                message.author.is_on_mobile() and
+                STATUS_OFFLINE in str(desktop_status) and 
+                STATUS_OFFLINE in str(web_status)
+                )
 
     # Avoid infinite loop self response
     if (message.author == client.user):
@@ -29,11 +42,17 @@ async def on_message(message):
     if (message.channel.name == ATTENDENCE_CHANNEL):
 
         if ( user_message.lower() == STATUS[0]):
-            insert_attendence(username, datetime.now(), STATUS[0])
+
+            # Checking if someone post in mobile device
+            if IS_JUST_ONLINE_IN_MOBILE:
+                await message.delete()
+                return await message.channel.send(f"{message.author.mention} trató de marcar asistencia desde el celular en home office. Se está haciendo wey xd!")
+
+            # insert_attendence(username, datetime.now(), STATUS[0])
             await message.add_reaction(COFFEE_EMOJI)
             return
 
         elif (user_message.lower() == STATUS[1]):
-            insert_attendence(username, datetime.now(), STATUS[1])
+            # insert_attendence(username, datetime.now(), STATUS[1])
             await message.add_reaction(COFFEE_EMOJI)
             return
